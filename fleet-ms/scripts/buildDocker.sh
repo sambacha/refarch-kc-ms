5f7be64171be66f8fb1e3e01fa736ff2233841e6
Edoardo Comar <ecomar@uk.ibm.com>
2019-01-22 10:53:08 +0000

    cleanup in buildDocker

--- fleet-ms/scripts/buildDocker.sh
+++ fleet-ms/scripts/buildDocker.sh
@@ -1,2 +1,4 @@
-mvn package
+find target -iname "*SNAPSHOT*" -print | xargs rm -rf
+rm -rf target/liberty/wlp/usr/servers/defaultServer/apps/expanded
+mvn install -DskipITs
 docker build -t ibmcase/kc-fleetms .
