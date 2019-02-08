5073d1688d4944ce4286b5952c587c961fb93228
Jerome Boyer <boyerje@us.ibm.com>
2019-02-08 10:04:47 -0800

    Fix a bug in build docker in voyages

--- voyages-ms/scripts/buildDocker.sh
+++ voyages-ms/scripts/buildDocker.sh
@@ -1,4 +1,9 @@
-. ./setenv.sh
+set p = $(echo $PWD | awk -v h="scripts" '$0 ~h')
+if [[ $PWD = */scripts ]]; then
+ cd ..
+fi
+. ./scripts/setenv.sh
+
 docker build -t ibmcase/$kname .
 # image for private registry in IBM Cloud
 docker tag ibmcase/$kname registry.ng.bluemix.net/ibmcaseeda/$kname 
--- voyages-ms/scripts/setenv.sh
+++ voyages-ms/scripts/setenv.sh
@@ -2,6 +2,6 @@
 export LDFLAGS=-L/usr/local/opt/openssl/lib
 export CPPFLAGS=-I/usr/local/opt/openssl/include
 export msname="voyages"
-export chart=$(ls ../chart/| grep $msname)
+export chart=$(ls ./chart/| grep $msname)
 export kname="kc-"$chart
 export ns="browncompute"
