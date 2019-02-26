0b107ae89389622ef9ab5731a2c627dba46a7e77
Jerome Boyer <boyerje@us.ibm.com>
2019-02-25 20:43:42 -0800

    chgt build process for fleet to use javatools docker image

--- voyages-ms/scripts/buildDocker.sh
+++ voyages-ms/scripts/buildDocker.sh
@@ -2,12 +2,14 @@
 echo "##########################################"
 echo " Build Voyage microservice "
 echo "##########################################"
-set p = $(echo $PWD | awk -v h="scripts" '$0 ~h')
+
 if [[ $PWD = */scripts ]]; then
  cd ..
 fi
 . ./scripts/setenv.sh
 
 docker build -t ibmcase/$kname .
-# image for private registry in IBM Cloud
-docker tag ibmcase/$kname registry.ng.bluemix.net/ibmcaseeda/$kname 
+if [[ $kcenv -ne "local"]]; then
+    # image for private registry in IBM Cloud
+    docker tag ibmcase/$kname registry.ng.bluemix.net/ibmcaseeda/$kname 
+fi
