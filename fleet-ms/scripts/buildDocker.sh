ca744cfb2fba625bd8a33080b85eeb8e7135703d
Jerome Boyer <boyerje@us.ibm.com>
2019-02-25 18:57:41 -0800

    change build process to use docker to build

--- fleet-ms/scripts/buildDocker.sh
+++ fleet-ms/scripts/buildDocker.sh
@@ -5,15 +5,27 @@ set p = $(echo $PWD | awk -v h="scripts" '$0 ~h')
 if [[ $PWD = */scripts ]]; then
  cd ..
 fi
+
 . ./scripts/setenv.sh
-if [ -f ../../../refarch-kc/certs/es-cert.pem ] 
+if [[ $kcenv -ne "local" && -f ../../../refarch-kc/certs/es-cert.pem ]]
 then
    openssl x509 -in ../../../refarch-kc/certs/es-cert.pem -inform pem -out es-cert.der -outform der
 fi
+
+
+
 find target -iname "*SNAPSHOT*" -print | xargs rm -rf
 rm -rf target/liberty/wlp/usr/servers/defaultServer/apps/expanded
-mvn install -DskipITs
+tools=$(docker images | grep javatools)
+if [[ -z "$tools" ]]
+then
+   mvn install -DskipITs
+else
+   docker run -v $(pwd):/home -ti ibmcase/javatools bash -c "cd /home && mvn install -DskipITs"
+fi
 # image for public docker hub
 docker build -t ibmcase/kc-fleetms .
-# image for private registry in IBM Cloud
-docker tag ibmcase/kc-fleetms registry.ng.bluemix.net/ibmcaseeda/kc-fleetms 
+if [[ $kcenv -ne "local"]]; then
+   #   image for private registry in IBM Cloud
+   docker tag ibmcase/kc-fleetms registry.ng.bluemix.net/ibmcaseeda/kc-fleetms 
+fi
--- fleet-ms/scripts/setenv.sh
+++ fleet-ms/scripts/setenv.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set p = $(echo $PWD | awk -v h="scripts" '$0 ~h')
+
 if [[ $PWD = */scripts ]]; then
  cd ..
 fi
@@ -7,3 +7,4 @@ export msname="fleetms"
 export chart=$(ls ./chart/| grep $msname)
 export kname="kc-"$chart
 export ns="browncompute"
+
